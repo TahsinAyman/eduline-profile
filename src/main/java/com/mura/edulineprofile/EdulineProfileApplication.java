3f951927bfbcfcd11bdb75fb3cf6621ba7128bc6
TahsinAyman <mail4tahsin@gmail.com>
2022-07-13 08:19:52 +0600

    init

---
+++ src/main/java/com/mura/edulineprofile/EdulineProfileApplication.java
@@ -0,0 +1,13 @@
+package com.mura.edulineprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdulineProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdulineProfileApplication.class, args);
+	}
+
+}
